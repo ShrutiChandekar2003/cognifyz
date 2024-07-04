d2d0f94934f876d5755f918708a9b3eb8a43df27
ShrutiChandekar2003 <shrutichandekar83@gmail.com>
2024-07-04 16:29:33 +0530

    initial commit

---
+++ contact form/btn.js
@@ -0,0 +1,7 @@
+const btn = Button.querySelector('.btn');
+btn.addEventListener('click',() =>{
+    
+    btn.style.backgroundcolor = '#ffac09';
+    btn.style.boxshadow  = ' 0 0 40px #ffac09';
+    
+    });
---
+++ contact form/script.js
@@ -0,0 +1,4 @@
+btn.addEventListener('click',() =>{
+btn.style.backgroundcolor = '#ffac09';
+
+});
